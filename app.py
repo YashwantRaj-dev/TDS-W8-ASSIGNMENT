a5b2a2bc9324b92c94272c1f806c976f497cb366
YashwantRaj-dev <119239914+YashwantRaj-dev@users.noreply.github.com>
2022-11-28 11:22:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.header("Week 8 Assignment- YASHWANT RAJ - 21f1001547")
 
-number1 = st.number_input('Insert First Number',value=0) 
+number1 = st.number_input('Insert First Number',value=0.0) 
 
-number2 = st.number_input('Insert Second Number',value=0)
+number2 = st.number_input('Insert Second Number',value=0.0)
 
 if number1 is not None and number2 is not None:
     st.write("Subtraction of the numbers is: {}".format((number1-number2)))
